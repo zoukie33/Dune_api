f8e283ee4dbe9765e33feefe018617bf9d2c5f71
zoukie33 <gadrat.romain33@gmail.com>
2018-05-11 11:55:41 +0200

    editing post for an studient

--- routes/eleves.js
+++ routes/eleves.js
@@ -29,7 +29,7 @@ router.get('/:id?', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 	var postData = req.body;
-	res.locals.connection.query('INSERT INTO d_eleves (nomEleve, prenomEleve) VALUES ?', postData, function(error, results, fields) {
+	res.locals.connection.query('INSERT INTO d_eleves (nomEleve, prenomEleve) VALUES ?' + postData, function(error, results, fields) {
 		if (error){
 			res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
 		} else {
@@ -40,4 +40,4 @@ router.post('/', function(req, res, next) {
 
 module.exports = router;
 
-// {"nomEleve":"la","prenomEleve":chatte}
+// {"nomEleve""la","prenomEleve":chatte}
