2a4ff8a767bfea53de99d3d7687a5b1a38cacf4b
Romain Gadrat <gadrat.romain33@gmail.com>
2018-09-12 17:20:42 +0200

    SSAS

--- routes/login.js
+++ routes/login.js
@@ -7,10 +7,13 @@ router.get('/prof/:mail?/:pass?', function(req, res, next) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
 	  		//If there is error, we send the error in the error section with 500 status
-	  	} else {
-  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	  	} else if (req.params.pass == "pass"){
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": "Correct account"}));
   			//If there is no error, all is good and response is 200OK.
 	  	}
+      else {
+        res.send(JSON.stringify({"status": 500, "error": error, "response": "LOGIN ERROR"}));
+      }
   	});
 });
 
