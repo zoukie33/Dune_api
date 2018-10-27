de96bb126dd4f7e50d6c3c1a6e438683ca5ed9bc
Romain Gadrat <gadrat.romain33@gmail.com>
2018-10-27 04:39:26 +0200

    (ADD/EDIT) - Adding update for student - Adding logs for student routes - Cleaning config file

--- config.js
+++ config.js
@@ -3,16 +3,3 @@ module.exports={
   sender: 'dune.epitech.contact@gmail.com',
   emailPass: 'fnbxfzmxfn33',
 };
-
-
-// mailer.extend(express, {
-//   from: 'noreply@dune.com',
-//   host: 'smtp.gmail.com', // hostname
-//   secureConnection: true, // use SSL
-//   port: 465, // port for secure SMTP
-//   transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
-//   auth: {
-//   user: 'dune.epitech.contact@gmail.com', // gmail id
-//   pass: 'fnbxfzmxfn33' // gmail password
-//   }
-// });
--- routes/eleves.js
+++ routes/eleves.js
@@ -19,10 +19,8 @@ router.get('/:id?', function(req, res, next) {
 	req.mysql.query('SELECT * from d_eleves WHERE idEleve = ' + req.params.id , function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
   			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  			//If there is no error, all is good and response is 200OK.
 	  	}
   	});
 });
@@ -44,60 +42,28 @@ router.post('/add', function(req, res, next) {
 			res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
 		} else {
 			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-      console.log("Un Prof a été ajouté: " + postData);
+      console.log("Un Prof a été ajouté : [" + nom + " - " + prenom + "]");
 		}
 	  res.end(JSON.stringify(results));
 	});
 });
 
 router.post('/update', function(req, res, next) {
-  res.json({"Error" : true, "Message" : "This route is not working now"});
-  /*var query = "SELECT * FROM ?? WHERE ??=?";
-  var table = ["d_profs", "emailProf", req.body.email];
-  query = mysql.format(query,table);
+	var id  = req.body.idEleve;
+  var nom  = req.body.nomEleve;
+  var prenom  = req.body.prenomEleve;
 
-  req.mysql.query(query,function(err,rows){
-  if(err) {
-    res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-  }
-  else {
-    if(rows.length==0){
-      var password = generator.generate({
-        length: 8,
-        numbers: true
-      });
-      var directorId = req.body.directorId;
-    	var postData = {
-        nomProf:req.body.nom,
-        prenomProf:req.body.prenom,
-        emailProf:req.body.email,
-        pass: md5(password),
-        access_token:"n/a",
-        device_type:"web"
-      }
-    	console.log(postData);
+  var query = "UPDATE d_eleves SET nomEleve = '"+ nom +"', prenomEleve = '"+ prenom +"' WHERE idEleve = " + id;
 
-      var query = "UPDATE ?? SET ?";
-      var table = ["d_profs"];
-      query = mysql.format(query,table);
-
-    	req.mysql.query(query, postData, function(error, results, fields) {
-    		if (error){
-    			res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-    		} else {
-    			res.send(JSON.stringify({"status": 200, "error": null, "pass": password}));
-          console.log("Un Prof a été ajouté: " + postData);
-    		}
-    	  res.end(JSON.stringify(results));
-    	});
+  req.mysql.query(query, function(error, results, fields) {
+    if (error){
+      res.send(JSON.stringify({"status": 500, "error": error, "response": "Impossible de mettre a jour cet élève."}));
+    } else {
+      res.send(JSON.stringify({"status": 200, "response": "Student Updated"}));
+      console.log("Un élève a été mis a jour : [" + id + " - " + nom + " - " + prenom + "]");
     }
-    else {
-      res.send(JSON.stringify({"status": 501, "error": err, "response": "Un utilisateur est déja inscrit avec cet Email."}));
-    }
-  }
-});*/
+    res.end(JSON.stringify(results));
+  });
 });
 
 module.exports = router;
-
-// {"nomEleve""la","prenomEleve":chatte}
