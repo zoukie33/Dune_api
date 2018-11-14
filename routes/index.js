151e9fe6fd8d311fb9d68fe1452bc93114d1f1bb
Romain Gadrat <gadrat.romain33@gmail.com>
2018-11-14 17:42:12 +0100

    (EDIT) Editing the documentation

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dune-Api', helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
+  res.render('index', { helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
 });
 
 router.get('/doc/get', function(req, res, next) {
-  res.render('get', { title: 'Dune-Api', helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
+  res.render('get', { helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
 });
 
 router.get('/doc/post', function(req, res, next) {
-  res.render('post', { title: 'Dune-Api', helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
+  res.render('post', { helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
 });
 
 router.get('/doc/helpCodes', function(req, res, next) {
-  res.render('helpCodes', { title: 'Dune-Api', helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
+  res.render('helpCodes', { helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
 });
 
 router.get('/doc/typeUsers', function(req, res, next) {
-  res.render('typeUsers', { title: 'Dune-Api', helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
+  res.render('typeUsers', { helpCodes: 'Status Codes', typeUser: 'Type utilisateur', post: 'Routes Post', get: 'Routes Get' });
 });
 
 module.exports = router;
