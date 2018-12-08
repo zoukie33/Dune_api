27fbecdb2f7cafbaa1c40478742f2cf4c0e13876
Romain Gadrat <gadrat.romain33@gmail.com>
2018-12-08 01:56:29 +0100

    (oubli)

--- routes/trombi.js
+++ routes/trombi.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   var idUser = req.body.idUser;
   var typeUser = req.body.typeUser;
+  console.log("req /trombi/ : " + req.body.idUser + " " + req.body.typeUser);
 
   if (typeUser && idUser) {
     if (typeUser == 1) {
@@ -27,12 +28,12 @@ router.post('/', function(req, res, next) {
 router.post('/classes', function(req, res, next) {
   var idUser = req.body.idUser;
   var typeUser = req.body.typeUser;
-
+  console.log("req /trombi/classes/ : " + req.body.idUser + " " + req.body.typeUser);
   if (typeUser && idUser) {
     if (typeUser == 1) {
-      var myQuery = 'SELECT c.idClasse FROM d_classe AS c, d_users AS u, d_profsAppClasse AS ac WHERE u.idUser = ac.idProf AND ac.idClasse = c.idClasse AND u.idUser = ' + idUser;
+      var myQuery = 'SELECT c.idClasse, c.label FROM d_classe AS c, d_users AS u, d_profsAppClasse AS ac WHERE u.idUser = ac.idProf AND ac.idClasse = c.idClasse AND u.idUser = ' + idUser;
     } else {
-      var myQuery = 'SELECT c.idClasse FROM d_classe AS c, d_users AS u, d_classeEcole AS ce2, d_profsAppEcole AS ae WHERE u.idUser = ae.idProf AND ae.idEcole = ce2.idEcole AND ce2.idClasse = c.idClasse AND u.idUser = ' + idUser;
+      var myQuery = 'SELECT c.idClasse, c.label FROM d_classe AS c, d_users AS u, d_classeEcole AS ce2, d_profsAppEcole AS ae WHERE u.idUser = ae.idProf AND ae.idEcole = ce2.idEcole AND ce2.idClasse = c.idClasse AND u.idUser = ' + idUser;
     }
     req.mysql.query(myQuery, function (error, results, fields) {
   	  	if(error){
@@ -50,12 +51,13 @@ router.post('/byClasse', function(req, res, next) {
   var idUser = req.body.idUser;
   var typeUser = req.body.typeUser;
   var idClasse = req.body.idClasse;
+  console.log("req /trombi/byClasse/ : " + req.body.idUser + " " + req.body.typeUser + " " + req.body.idClasse);
 
   if (typeUser && idUser && idClasse) {
     if (typeUser == 1) {
       var myQuery = 'SELECT e.* FROM d_classe AS c, d_users AS u, d_profsAppClasse AS ac, d_classeEleve as ce, d_eleves as e WHERE u.idUser = ac.idProf AND ac.idClasse = c.idClasse AND c.idClasse = ce.idClasse AND ce.idEleve = e.idEleve AND u.idUser = ' + idUser + ' AND c.idClasse = ' + idClasse;
     } else {
-      var myQuery = 'SELECT e.*, ce.idClasse FROM d_eleves AS e, d_classeEleve AS ce, d_classe AS c, d_users AS u, d_classeEcole AS ce2, d_profsAppEcole AS ae WHERE u.idUser = ae.idProf AND ae.idEcole = ce2.idEcole AND ce2.idClasse = c.idClasse AND c.idClasse = ce.idClasse AND ce.idEleve = e.idEleve AND u.idUser = ' + idUser + ' AND c.idClasse = ' + iClasse;
+      var myQuery = 'SELECT e.*, ce.idClasse FROM d_eleves AS e, d_classeEleve AS ce, d_classe AS c, d_users AS u, d_classeEcole AS ce2, d_profsAppEcole AS ae WHERE u.idUser = ae.idProf AND ae.idEcole = ce2.idEcole AND ce2.idClasse = c.idClasse AND c.idClasse = ce.idClasse AND ce.idEleve = e.idEleve AND u.idUser = ' + idUser + ' AND c.idClasse = ' + idClasse;
     }
     req.mysql.query(myQuery, function (error, results, fields) {
   	  	if(error){
